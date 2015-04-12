daaa63e9bb6e15c246c7385e8a30dc37e58fc7f2
mikeatgl <speakoftrees@gmail.com>
2015-04-12 03:39:15 -0700

    Initial commit.

---
+++ index.js
@@ -0,0 +1,19 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/views/index.html');
+});
+
+io.on('connection', function(socket){
+	var userName = 'Anonymous';
+	socket.broadcast.emit('newUser', 'A new user has connected.');
+	socket.on('chat message', function(msg, usr){
+		io.emit('chat message', msg, usr);
+	});
+});
+
+http.listen(3000, function(){
+	console.log('listening on *:3000');
+});
